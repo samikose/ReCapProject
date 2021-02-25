63f26eb6bb152915601ab92912b4bca4486fb5aa
Sami Köse <samikose1227@hotmail.com>
2021-02-25 23:46:41 +0300

    12.Gün

---
+++ Business/ValidationRules/FluentValidation/BrandValidator/BrandValidator.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation.BrandValidator
+{
+    public class BrandValidator:AbstractValidator<Brand>
+    {
+        public BrandValidator()
+        {
+            RuleFor(b => b.BrandName).MinimumLength(2).WithMessage("Marka adı en az 3 karakter uzunluğunda olmalıdır");
+        }
+    }
+}
---
+++ Business/ValidationRules/FluentValidation/ColorValidator/ColorValidator.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation.ColorValidator
+{
+    public class ColorValidator:AbstractValidator<Color>
+    {
+        public ColorValidator()
+        {
+            RuleFor(c => c.ColorName).MinimumLength(2).WithMessage("Renk adı en az 2 karakter uzunluğunda olmalıdır");
+        }
+    }
+}
